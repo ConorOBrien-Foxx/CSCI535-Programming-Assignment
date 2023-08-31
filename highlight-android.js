9bfce5894df4052cd11624ba860c686b5cda6993
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2023-08-30 20:07:11 -0400

    abstract to handle multiple images at once

--- highlight-android.js
+++ highlight-android.js
@@ -57,6 +57,92 @@ const parseBoolean = boolString => {
     }
     throw new Error(`Malformed boolean: ${boolean}`);
 };
+    
+const drawHighlightBox = (ctx, topLeftX, topLeftY, bottomRightX, bottomRightY) => {
+    ctx.setLineDash([25, 25]);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "red";
+    let shapeWidth = bottomRightX - topLeftX;
+    let shapeHeight = bottomRightY - topLeftY;
+    ctx.rect(topLeftX, topLeftY, shapeWidth, shapeHeight);
+    ctx.stroke();
+    ctx.setLineDash([]);
+};
+
+const addHighlightBoxes = (xmlDoc, img) => {
+    const workCanvas = document.createElement("canvas");
+    const ctx = workCanvas.getContext("2d");
+    workCanvas.width = img.width;
+    workCanvas.height = img.height;
+    ctx.drawImage(img, 0, 0, workCanvas.width, workCanvas.height);
+    
+    xmlDoc.querySelectorAll("node").forEach(node => {
+        const isLeaf = node.childElementCount === 0;
+        if(!isLeaf) {
+            return;
+        }
+        
+        const bounds = node.getAttribute("bounds");
+        if(!bounds) {
+            return;
+        }
+        
+        const matchData = bounds.match(/\[(\d+),(\d+)\]\[(\d+),(\d+)\]/);
+        if(!matchData) {
+            console.error("Malformed bounds data: ", bounds);
+            return;
+        }
+        const [ topLeftX, topLeftY, bottomRightX, bottomRightY ] = matchData
+            .slice(1)
+            .map(datum => parseInt(datum, 10));
+        
+        drawHighlightBox(ctx, topLeftX, topLeftY, bottomRightX, bottomRightY);
+    });
+    
+    const outputDataUrl = workCanvas.toDataURL("image/png");
+    
+    return outputDataUrl;
+};
+
+const ONLY_EXTENSION = /^\.[^.]+$/;
+const groupFileNames = (...fileGroups) => {
+    fileGroups = fileGroups.map(group => Array.from(group));
+    let names = [...fileGroups[0]]
+        .map(file => file.name)
+        // https://stackoverflow.com/a/47956767/4119004
+        .map(fileName => fileName.substring(0, fileName.lastIndexOf(".")) || fileName);
+    
+    let fileCollection = {};
+    for(let name of names) {
+        fileCollection[name] = [];
+        for(let group of fileGroups) {
+            let targetFile = group.find(file => file.name.startsWith(name)
+                && ONLY_EXTENSION.test(file.name.slice(name.length))
+            );
+            targetFile ??= null;
+            fileCollection[name].push(targetFile);
+        }
+    }
+    
+    return names.map(name => ({
+        name,
+        files: fileCollection[name],
+    }));
+};
+
+const generateHighlightsImage = async (xmlFile, imageFile) => {
+    const xmlString = await readTextFileContent(xmlFile);
+    const xmlDoc = parseXmlString(xmlString);
+
+    const imageDataUrl = await readImageFileDataUrl(imageFile);
+    const img = await loadImageFromDataUrl(imageDataUrl);
+
+    const outputUrl = addHighlightBoxes(xmlDoc, img);
+    const outputImage = document.createElement("img");
+    outputImage.src = outputUrl;
+
+    return outputImage;
+};
 
 window.addEventListener("load", function () {
     
@@ -64,57 +150,18 @@ window.addEventListener("load", function () {
         const screenshotUpload = app.querySelector(".screenshot-upload");
         const xmlUpload = app.querySelector(".xml-upload");
         const submitButton = app.querySelector(".submit");
+        const gallery = app.querySelector(".gallery");
         
         submitButton.addEventListener("click", async function () {
+            const xmlImagePairs = groupFileNames(xmlUpload.files, screenshotUpload.files);
             
-            // TODO: separate multiple uploaded images/xml into pairs by common prefix
-            const imageFile = screenshotUpload.files[0];
-            const xmlFile = xmlUpload.files[0];
-            
-            const xmlString = await readTextFileContent(xmlFile);
-            const xmlDoc = parseXmlString(xmlString);
-            console.log(xmlDoc);
-            
-            const imageDataUrl = await readImageFileDataUrl(imageFile);
-            const img = await loadImageFromDataUrl(imageDataUrl);
-            
-            const workCanvas = document.createElement("canvas");
-            const ctx = workCanvas.getContext("2d");
-            workCanvas.width = img.width;
-            workCanvas.height = img.height;
-            ctx.drawImage(img, 0, 0, workCanvas.width, workCanvas.height);
-            
-            const drawOutline = (topLeftX, topLeftY, bottomRightX, bottomRightY) => {
-                ctx.setLineDash([25, 25]);
-                ctx.lineWidth = 5;
-                ctx.strokeStyle = "red";
-                let shapeWidth = bottomRightX - topLeftX;
-                let shapeHeight = bottomRightY - topLeftY;
-                ctx.rect(topLeftX, topLeftY, shapeWidth, shapeHeight);
-                ctx.stroke();
-                ctx.setLineDash([]);
-            };
-            
-            xmlDoc.querySelectorAll("node").forEach(node => {
-                const bounds = node.getAttribute("bounds");
-                if(!bounds) {
-                    return;
-                }
+            xmlImagePairs.forEach(async data => {
+                const [ xmlFile, imageFile ] = data.files;
                 
-                const matchData = bounds.match(/\[(\d+),(\d+)\]\[(\d+),(\d+)\]/);
-                if(!matchData) {
-                    console.error("Malformed bounds data: ", bounds);
-                    return;
-                }
-                const [ topLeftX, topLeftY, bottomRightX, bottomRightY ] = matchData
-                    .slice(1)
-                    .map(datum => parseInt(datum, 10));
-                
-                drawOutline(topLeftX, topLeftY, bottomRightX, bottomRightY);
+                const highlights = await generateHighlightsImage(xmlFile, imageFile);
+                gallery.appendChild(highlights);
             });
             
-            
-            app.appendChild(workCanvas);
         });
         
         // TODO: remove; convenience
